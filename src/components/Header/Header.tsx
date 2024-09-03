db498c5ec6bb32473a8869aecf84d717231f4e0f
AbolfazlKhosravi <abolfazlkhosravi.2003jun@gmail.com>
2024-09-03 10:50:42 +0330

    Creating  controle function for search and static guest option item

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,20 @@
-import { HiCalendar, HiSearch } from "react-icons/hi";
+import React, { useState } from "react";
+import { HiCalendar, HiMinus, HiPlus, HiSearch } from "react-icons/hi";
 import { MdLocationOn } from "react-icons/md";
 
 const Header = () => {
+  const [destination, setDestination] = useState<string>("");
+  const [openOptions, setOpenOptions] = useState<boolean>(false);
   return (
     <div className="header">
       <div className="headerSearch">
         <div className="headerSearchItem">
           <MdLocationOn className="headerIcon locationIcon" />
           <input
+            value={destination}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setDestination(e.target.value)
+            }
             type="text"
             placeholder="Where to go?"
             className="headerSearchInput"
@@ -22,7 +29,10 @@ const Header = () => {
           <span className="seperator"></span>
         </div>
         <div className="headerSearchItem">
-          <div id="optionDropDown">1 adullt &bull; 2 children &bull; 1rome</div>
+          <div id="optionDropDown" onClick={() => setOpenOptions(!openOptions)}>
+            1 adullt &bull; 2 children &bull; 1rome
+          </div>
+          {openOptions && <GuestOptionList />}
           <span className="seperator"></span>
         </div>
         <div className="headerSearchItem">
@@ -34,4 +44,28 @@ const Header = () => {
     </div>
   );
 };
+
+const GuestOptionList = () => {
+  return (
+    <div className="guestOptions">
+      <OptionItem/>
+    </div>
+  );
+};
+const OptionItem = () => {
+  return ( <div className="guestOptionItem">
+    <span className="optionText">Adult</span>
+    <div className="optionCounter">
+      <button className="optionCounterBtn">
+        <HiMinus className="icon" />
+      </button>
+      <span className="optionCounterNumber">2</span>
+      <button className="optionCounterBtn">
+        <HiPlus className="icon" />
+      </button>
+    </div>
+  </div> );
+}
+ 
+
 export default Header;
