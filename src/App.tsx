396b3ec46ecac0bee11806299746c46ceecb202b
AbolfazlKhosravi <abolfazlkhosravi.2003jun@gmail.com>
2024-09-06 22:06:25 +0330

    add useFetch custom function for request to the backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import Header from "./components/Header/Header";
-
+import  { Toaster } from 'react-hot-toast';
 function App() {
   return (
     <div>
       <Header />
       <p>hey it me!</p>
+      <Toaster/>
     </div>
   );
 }
---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+interface FetchResult<T> {
+  data: T[];
+  isLoading: boolean;
+}
+
+function useFetch<T>(URL: string, query: string): FetchResult<T> {
+  const [data, setData] = useState<Array<T>>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const response = await axios.get<T[]>(`${URL}?${query}`);
+        setData(response.data);
+      } catch (error) {
+        setData([]);
+        if (error instanceof Error) {
+          toast.error(error?.message || "An error occurred.");
+        } else {
+          console.error("An unknown error occurred");
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, [URL, query]);
+
+  return { data, isLoading };
+}
+
+export default useFetch;
